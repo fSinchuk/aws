31c4697ba96427c22196213b4d4b2edbd45c3de9
fSinchuk <51887269+fSinchuk@users.noreply.github.com>
2019-11-20 19:44:20 +0100

    Init commit

---
+++ SQS/SQS.Reader/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace SQS.Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var awsCredentials = new Amazon.Runtime.BasicAWSCredentials(configuration["seecretAccess"], configuration["seecretKey"]);
+            var sqsClient = new AmazonSQSClient(awsCredentials, RegionEndpoint.APSoutheast2);
+
+            var queueUrl = sqsClient.GetQueueUrlAsync("EmailQueue").Result.QueueUrl;
+
+            var reciveMessageRequest = new ReceiveMessageRequest {
+                QueueUrl = queueUrl
+            };
+
+            var reciveMessageResponse = sqsClient.ReceiveMessageAsync(reciveMessageRequest).Result;
+            foreach (var message in reciveMessageResponse.Messages) {
+                Console.WriteLine("Message");
+                
+                Console.WriteLine($"MessageId {message.MessageId}");
+                Console.WriteLine($"Body {message.Body}");
+            }
+
+            var messageReceptHandler = reciveMessageResponse.Messages.FirstOrDefault()?.ReceiptHandle;
+            var deleteRequest = new DeleteMessageRequest {
+                QueueUrl = queueUrl,
+                ReceiptHandle = messageReceptHandler
+            };
+
+            var result = sqsClient.DeleteMessageAsync(deleteRequest).Result.HttpStatusCode;
+
+            Console.WriteLine($"Message deleted {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SQS/SQS.Writer/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine(new string('*', 100));
+            Console.WriteLine("SQS Service");
+            Console.WriteLine(new string('*', 100));
+
+            var awsCredentials = new Amazon.Runtime.BasicAWSCredentials(configuration["seecretAccess"], configuration["seecretKey"]);
+            IAmazonSQS sqsClient = new AmazonSQSClient(awsCredentials, RegionEndpoint.APSoutheast2);
+            Console.WriteLine("Create a queue called EmailQueue");
+
+            var sqsRequest = new CreateQueueRequest { QueueName = "EmailQueue" };
+            var createQueueReponse = sqsClient.CreateQueueAsync(sqsRequest).Result;
+
+            var sqsUrl = createQueueReponse.QueueUrl;
+
+            var listQueueRequest = new ListQueuesRequest();
+            var listQueueResponse = sqsClient.ListQueuesAsync(listQueueRequest);
+
+            foreach (var item in listQueueResponse.Result.QueueUrls)
+            {
+                Console.WriteLine($"Queue url: {item}");
+            }
+
+            Console.WriteLine("Send a message to Queue");
+
+            var sqsMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = sqsUrl,
+                MessageBody = "First message"
+            };
+
+            sqsClient.SendMessageAsync(sqsMessageRequest);
+
+            Console.WriteLine("Message sent");
+            Console.ReadKey();
+        }
+    }
+}
